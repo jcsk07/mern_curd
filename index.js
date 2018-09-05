5ba1ce598e380f0a4d80ecf2460e91e45f05e9fb
jcsk07 <sekhar.chandra712@gmail.com>
2018-09-05 23:33:52 +0530

    express and mongodb data added

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ app.get('/pandu',function(req,res){
 	res.send("hello this is the pandu page"+Date.now());
 });
 
-app.post('/pandu',function(req,res){
-	console.log(req.body);
-	res.send(req.body+"this is the sended json data");
+app.post('/pandu',function(req,res){ //the json data is present in the body of the request
+	console.log(req.body.name);//we are printing the json data with a specific variable 
+    res.json(req.body);//sending the json data as a response
 	console.log("this is the post request of the json data");
 });
 app.listen(3000);
