d7e0b29c61ca7c33c6565713a14bcffbec926cd3
jcsk07 <sekhar.chandra712@gmail.com>
2018-09-07 23:57:27 +0530

    mongodb creud completion and express routing to html pages

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
-
 var bodyParser=require('body-parser');
 app.use(bodyParser.json());//used to use the json data comming
+app.use(express.urlencoded());
+var mongoose=require('mongoose');//used to connect the mongodb 
+mongoose.connect('mongodb://localhost/my_db_crud', { useNewUrlParser: true });//used to connect the mongodb databes named:"my_db_crud"
+
+var path=require('path');
 
 app.get('/', function(req, res){
 	console.log("visit:- http://localhost:3000")
@@ -20,4 +24,18 @@ app.post('/pandu',function(req,res){ //the json data is present in the body of t
     res.json(req.body);//sending the json data as a response
 	console.log("this is the post request of the json data");
 });
+
+app.get('/studentform',function(req,res){
+	res.sendFile(path.join(__dirname + '/html_pages/student_form.html'));
+});
+
+app.post('/studentform',function(req,res){
+	var person = req.body;
+	res.send(person.textnames);
+	console.log(JSON.stringify(req.body));
+	console.log("this is the post request of student form");
+}); 
+//---------TO CREATE A COLLECTION WITH PREDEFINED FIELDS WE USE THE FOLLOWING---------------
+//NOTE:- we call collection as the model in this 
+
 app.listen(3000);
