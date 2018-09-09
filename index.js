b6803a7f94a5c5377d80687a28252488c7c8cfd9
jcsk07 <sekhar.chandra712@gmail.com>
2018-09-09 23:49:23 +0530

    express CREATE AND READ completed

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ var bodyParser=require('body-parser');
 app.use(bodyParser.json());//used to use the json data comming
 app.use(bodyParser.urlencoded({extended: true}));
 var path=require('path');
-
-
+var Mongodb=require('mongodb');
 
 //---------TO CREATE A COLLECTION WITH PREDEFINED FIELDS WE USE THE FOLLOWING---------------
 //NOTE:- we call collection as the model in this 
@@ -13,7 +12,6 @@ var mongoose=require('mongoose');//used to connect the mongodb
 mongoose.connect('mongodb://localhost/my_db_crud', { useNewUrlParser: true });//used to connect the mongodb databes named:"my_db_crud"
 //TO CHECK WEATHER THE MONGODB CONNECTION IS PRESENT OR NO WE USE THE FLG:-
 var db = mongoose.connection;
-
 
 //declaring a schema for a collection
 var studentSchema = new mongoose.Schema({  //we are creating the Schema of collection with different fields
@@ -37,8 +35,6 @@ db.once('open', function() {
 
 app.post('/studentform',function(req,res){
 	var person = req.body; //taking the json data into the person
-	
-
 	var stu = new stdetails({  //creating a document for databese with the predeclared mode
 		textnames : person.textnames, //assigning the data from person to the stu using dot operator
 		fathername : person.fathername,
@@ -49,14 +45,17 @@ app.post('/studentform',function(req,res){
 	});
 	//saving the student details as follows:-
 	stu.save(function(err,stu_details){
-		if(err)
+		if(err){
 			console.log("Failed to Insert the data");
-		else
+			res.send("insertion Failed");
+		}
+		else{
 			console.log("Successfully inserted document");
+			res.send("insertion successful");
+		}
 
 	});
-	res.send(person.textnames);
-	console.log(JSON.stringify(req.body));
+	
 	console.log("this is the post request of student form");
 }); 
 
@@ -84,7 +83,27 @@ app.get('/studentform',function(req,res){
 	res.sendFile(path.join(__dirname + '/html_pages/student_form.html'));
 });
 
+app.get('/retrive',function(req,res){
+	stdetails.find(function(err,response){
+		res.json(response);
+	});
+});
+
+app.get('/find',function(req,res){
+	stdetails.find({textnames:"deepika",fathername:"ramu"},'textnames fathername',function(err,docs){
+		res.json(docs);
+	});
+});
 
 
+//---------------------UPDATING THE DOCUMENTS----------------------
+var up;
 
+app.get('/update',function(req,res){
+	res.sendFile(path.join(__dirname + '/html_pages/update_form1.html'));
+});
+
+app.post('/updateform',function(req,res){
+	up=req.body;
+});
 app.listen(3000);
